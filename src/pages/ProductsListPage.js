23eff2c6a89632215babe3550e46d4b737cb6025
David Y. Stephenson <david@davidystephenson.com>
2025-02-01 13:28:37 -0500

    Map products

--- src/pages/ProductsListPage.js
+++ src/pages/ProductsListPage.js
@@ -1,7 +1,14 @@
 import { Container } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
 
 export default function ProductsListPage () {
+  const cards = products.map(product => {
+    return <Product key={product.id} product={product} />
+  })
   return (
-    <Container>ProductsListPage</Container>
+    <Container>
+      {cards}
+    </Container>
   )
 }
