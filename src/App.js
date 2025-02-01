5dc1b1aaf4d34dedbf9035e8b0022f5ea5d074f5
David Y. Stephenson <david@davidystephenson.com>
2025-02-01 13:19:14 -0500

    Update app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage";
+import ProductsListPage from "./pages/ProductsListPage";
 
 const App = () => {
+  const localUser = localStorage.getItem('user')
+  const user = JSON.parse(localUser)
+  const page = user ? <ProductsListPage /> : <LoginPage />
   return (
     <>
       <Header />
-      <LoginPage />
+      {page}
       <Footer />
     </>
   );
